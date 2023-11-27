b116ee62f7aa1576983bad705d597092f69168de
Angela Lima <angelamcostalima@icloud.com>
2023-11-27 19:08:25 +0000

    ✨ add `utils`
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- src/utils.c
+++ src/utils.c
@@ -6,8 +6,43 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:17:23 by anlima            #+#    #+#             */
-/*   Updated: 2023/11/20 17:09:08 by anlima           ###   ########.fr       */
+/*   Updated: 2023/11/27 19:07:01 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
+
+int		get_flag(char *line);
+void	free_dptr(char **str);
+
+// t_win	*win(void)
+// {
+// 	static t_win	win;
+
+// 	return (&win);
+// }
+
+int	get_flag(char *line)
+{
+	int	i;
+	int	flag;
+
+	i = -1;
+	flag = 0;
+	while (line[++i])
+	{
+		if (!flag && ft_isalnum(line[i]))
+			flag = 1;
+	}
+	return (flag);
+}
+
+void	free_dptr(char **str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		free(str[i]);
+	free(str);
+}
