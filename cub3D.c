da324c5954ce542db3fe67e6c359ac76a0cc9c7f
Angela Lima <angelamcostalima@icloud.com>
2023-11-29 17:08:24 +0000

    💩 add tests to `main.c`
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- cub3D.c
+++ cub3D.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:08:27 by anlima            #+#    #+#             */
-/*   Updated: 2023/11/27 18:50:26 by anlima           ###   ########.fr       */
+/*   Updated: 2023/11/29 17:05:47 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,16 @@ int	main(int argc, char **argv)
 		return (write(2, ARG_ERROR, 49));
 	if (!read_args(argv[1]))
 		write(1, "error\n", 7);
+	for (int i = 0; i < 3; i++)
+	{
+		printf("ceiling[%i] -> %s\n", i, map()->ceiling[i]);
+		printf("floor[%i] -> %s\n", i, map()->floor[i]);
+	}
+	printf("NO -> %s\n", map()->no);
+	printf("SO -> %s\n", map()->so);
+	printf("EA -> %s\n", map()->ea);
+	printf("WE -> %s\n", map()->we);
+	for (int i = 0; i < map()->cols; i++)
+		printf("map[%i] ->\t%s\n", i, map()->map[i]);
 	return (1);
 }
