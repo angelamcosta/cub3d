90cbc234528eb176819a5edc9b0d20c4c2963bf1
Angela Lima <angelamcostalima@icloud.com>
2023-11-29 17:05:34 +0000

    🎨 add `check_rgb` to rgb.c file
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- src/parser/rgb.c
+++ src/parser/rgb.c
@@ -6,13 +6,28 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 15:43:25 by anlima            #+#    #+#             */
-/*   Updated: 2023/11/29 15:44:37 by anlima           ###   ########.fr       */
+/*   Updated: 2023/11/29 16:58:21 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
+int		check_rgb(char **args);
 void	save_rgb(char **rgb, char pos);
+
+int	check_rgb(char **args)
+{
+	int		flag;
+	char	**rgb;
+
+	flag = 0;
+	rgb = ft_split(args[1], ',');
+	flag = check_params(rgb);
+	if (flag)
+		save_rgb(rgb, *args[0]);
+	free_dptr(rgb);
+	return (flag);
+}
 
 void	save_rgb(char **rgb, char pos)
 {
@@ -22,11 +37,11 @@ void	save_rgb(char **rgb, char pos)
 	if (pos == 'C')
 	{
 		while (++i < 3)
-			map()->ceiling[i] = ft_atoi(rgb[i]);
+			map()->ceiling[i] = ft_strdup(rgb[i]);
 	}
 	else
 	{
 		while (++i < 3)
-			map()->floor[i] = ft_atoi(rgb[i]);
+			map()->floor[i] = ft_strdup(rgb[i]);
 	}
 }
