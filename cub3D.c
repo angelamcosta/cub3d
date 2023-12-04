2551ff5ddd5d90715774fb7a60654537d7d4b231
Angela Lima <angelamcostalima@icloud.com>
2023-12-04 21:33:17 +0000

    💩 add tests to `main`
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- cub3D.c
+++ cub3D.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:08:27 by anlima            #+#    #+#             */
-/*   Updated: 2023/12/01 16:26:24 by anlima           ###   ########.fr       */
+/*   Updated: 2023/12/04 21:13:02 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,21 @@ int	main(int argc, char **argv)
 	if (argc != 2 || !verify_map(argv[1]))
 		return (write(2, ARG_ERROR, 49));
 	if (!read_args(argv[1]))
-		write(1, "error\n", 7);
-	for (int i = 0; i < 3; i++)
-	{
-		printf("ceiling[%i] -> %s\n", i, map()->ceiling[i]);
-		printf("floor[%i] -> %s\n", i, map()->floor[i]);
-	}
-	printf("NO -> %s\n", map()->no);
-	printf("SO -> %s\n", map()->so);
-	printf("EA -> %s\n", map()->ea);
-	printf("WE -> %s\n", map()->we);
+		return (write(1, "error\n", 7));
 	set_row_col();
-	for (int i = 0; i < map()->rows; i++)
-		printf("map[%i] ->\t|%s|\n", i, map()->map[i]);
-	printf("DEBUG: rows => %i\tcols => %i\n", map()->rows, map()->cols);
-	printf("DEBUG: is it valid => %i\n", is_valid_map(map()->map, map()->rows, map()->cols));
-	for (int i = 0; i < map()->rows; i++)
-		printf("map[%i] ->\t|%s|\n", i, map()->map[i]);
-	return (1);
+	if (!is_map_closed() || !is_valid_path())
+		return (write(1, "error\n", 7));
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	printf("ceiling[%i] -> %s\n", i, map()->ceiling[i]);
+	// 	printf("floor[%i] -> %s\n", i, map()->floor[i]);
+	// }
+	// printf("NO -> %s\n", map()->no);
+	// printf("SO -> %s\n", map()->so);
+	// printf("EA -> %s\n", map()->ea);
+	// printf("WE -> %s\n", map()->we);
+	// for (int i = 0; i < map()->rows; i++)
+	// 	printf("map[%i] ->\t|%s|\n", i, map()->map[i]);
+	// printf("DEBUG: rows => %i\tcols => %i\n", map()->rows, map()->cols);
+	return (0);
 }
