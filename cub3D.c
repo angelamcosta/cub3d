6e57f489e1a14a69bba27f9841b545674ca9f899
Angela Lima <angelamcostalima@icloud.com>
2023-12-20 17:01:48 +0000

    💩 add tests to `main`
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- cub3D.c
+++ cub3D.c
@@ -6,11 +6,27 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:08:27 by anlima            #+#    #+#             */
-/*   Updated: 2023/12/14 14:24:15 by anlima           ###   ########.fr       */
+/*   Updated: 2023/12/20 16:53:29 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub3D.h"
+
+void	test_maps(void)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		printf("ceiling[%i] -> %s\n", i, map()->ceiling[i]);
+		printf("floor[%i] -> %s\n", i, map()->floor[i]);
+	}
+	printf("NO -> %s\n", map()->no);
+	printf("SO -> %s\n", map()->so);
+	printf("EA -> %s\n", map()->ea);
+	printf("WE -> %s\n", map()->we);
+	for (int i = 0; i < map()->rows; i++)
+		printf("map[%i] ->\t|%s|\n", i, map()->map[i]);
+	printf("DEBUG: rows => %i\tcols => %i\n", map()->rows, map()->cols);
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,17 +38,11 @@ int	main(int argc, char **argv)
 	set_row_col();
 	if (!is_map_closed() || !is_valid_path())
 		return (write(1, "error\n", 7));
-	// for (int i = 0; i < 3; i++)
-	// {
-	// 	printf("ceiling[%i] -> %s\n", i, map()->ceiling[i]);
-	// 	printf("floor[%i] -> %s\n", i, map()->floor[i]);
-	// }
-	// printf("NO -> %s\n", map()->no);
-	// printf("SO -> %s\n", map()->so);
-	// printf("EA -> %s\n", map()->ea);
-	// printf("WE -> %s\n", map()->we);
-	// for (int i = 0; i < map()->rows; i++)
-	// 	printf("map[%i] ->\t|%s|\n", i, map()->map[i]);
-	// printf("DEBUG: rows => %i\tcols => %i\n", map()->rows, map()->cols);
+	win()->mlx = mlx_init();
+	(win()->mlx_win) = mlx_new_window(win()->mlx, WIDTH, HEIGHT, "cub3D");
+	render_rect();
+	// mlx_pixel_put(win()->mlx, win()->mlx_win, x, y, color);
+	mlx_hook(win()->mlx_win, 2, 1L << 0, keyhooks, win);
+	mlx_loop(win()->mlx);
 	return (0);
 }
