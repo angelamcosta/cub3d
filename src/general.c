9b3fa0f58f44c77bed929e979a6fe96991300804
Angela Lima <angelamcostalima@icloud.com>
2023-12-20 16:59:59 +0000

    ✨ add `struct` functions
    * add functions to access the fields in the structs
    
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- src/general.c
+++ src/general.c
@@ -6,17 +6,49 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 14:22:21 by anlima            #+#    #+#             */
-/*   Updated: 2023/12/14 14:25:19 by anlima           ###   ########.fr       */
+/*   Updated: 2023/12/20 16:16:49 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
+t_map	*map(void);
+t_win	*win(void);
+t_img	*img(void);
 void	init_vars(void);
+void	finish_execution(void);
+
+t_map	*map(void)
+{
+	static t_map	map;
+
+	return (&map);
+}
+
+t_win	*win(void)
+{
+	static t_win	win;
+
+	return (&win);
+}
+
+t_img	*img(void)
+{
+	static t_img	img;
+
+	return (&img);
+}
 
 void	init_vars(void)
 {
 	map()->pos[0] = '-';
 	map()->pos[1] = '-';
 	map()->pos[2] = '-';
+}
+
+void	finish_execution(void)
+{
+	mlx_destroy_window(win()->mlx, win()->mlx_win);
+	clean_mallocs();
+	exit(0);
 }
