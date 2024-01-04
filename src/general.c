f44db747ae87cf1bddc2424332fb5277883451e3
Angela Lima <angelamcostalima@icloud.com>
2024-01-04 11:18:41 +0000

    🎨 refactor `general.c`
    * remove `t_img    *img` function
    
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- src/general.c
+++ src/general.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 14:22:21 by anlima            #+#    #+#             */
-/*   Updated: 2023/12/28 19:05:32 by anlima           ###   ########.fr       */
+/*   Updated: 2023/12/29 19:25:40 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 t_map	*map(void);
 t_win	*win(void);
-t_img	*img(void);
 t_pos	*pos(void);
 t_line	*line(void);
 
@@ -30,13 +29,6 @@ t_win	*win(void)
 	static t_win	win;
 
 	return (&win);
-}
-
-t_img	*img(void)
-{
-	static t_img	img;
-
-	return (&img);
 }
 
 t_pos	*pos(void)
