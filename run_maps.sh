5c599758795209df02506cf65756e76182b294ef
Angela Lima <angelamcostalima@icloud.com>
2024-01-18 18:47:14 +0000

    🔨  update `run_maps.sh` script
    Co-authored-by: Miguel Diogo <mgdiogo@users.noreply.github.com>

--- run_maps.sh
+++ run_maps.sh
@@ -4,7 +4,7 @@
 map_directory="maps/wrong"
 
 # Set the common output file
-common_output_file="all_maps.out"
+common_output_file="all_maps_valgrind.out"
 
 # Check if the directory exists
 if [ ! -d "$map_directory" ]; then
@@ -20,14 +20,14 @@ for map_file in "$map_directory"/*; do
         map_name=$(basename "$map_file")
         map_name="${map_name%.*}"
 
-        # Run cub3D with the map file and save the map name and output to the common file
-        echo "Running $map_name:" >> "$common_output_file"
-        ./cub3D "$map_file" >> "$common_output_file" 2>&1
+        # Run cub3D with valgrind and the map file, save the map name and output to the common file
+        echo "Running $map_name with valgrind:" >> "$common_output_file"
+        valgrind --leak-check=full --show-leak-kinds=all ./cub3D "$map_file" >> "$common_output_file" 2>&1
         echo -e "\n-------------------------------------------\n" >> "$common_output_file"
 
         # Print a message indicating the completion of the execution
-        echo "Finished running $map_name."
+        echo "Finished running $map_name with valgrind."
     fi
 done
 
-echo "All maps processed. Combined output saved to $common_output_file."
+echo "All maps processed with valgrind. Combined output saved to $common_output_file."
